a3ea450aaa2cef3afedb00edb4e712288bc131d6
Rahul Tripathi <rahultripathidev@gmail.com>
2024-05-19 14:08:55 +0530

    ft: rf and add err

--- src/tor_proxy.rs
+++ src/tor_proxy.rs
@@ -1,3 +1,4 @@
+use std::io;
 use std::io::{Read, Write};
 use std::net::{Ipv4Addr, TcpStream};
 use std::os::fd::AsRawFd;
@@ -5,48 +6,50 @@ use std::str::FromStr;
 
 use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
 
-const PROXY_ADDR: &str = "127.0.0.1";
-const PROXY_PORT: u32 = 9050;
+const UNAME: &[u8] = "TORALIZERS".as_bytes();
 
 
-pub fn proxy() {
-    let mut stream = TcpStream::connect(format!("{}:{}", PROXY_ADDR, PROXY_PORT)).unwrap();
-    stream.set_nodelay(true).expect("TODO: panic message");
-    let user_id: &[u8] = "W".as_bytes();
-    let mut socks4Frame: Vec<u8> = Vec::new();
-    socks4Frame.write_u8(4).expect("write VN");
-    socks4Frame.write_u8(1).expect("write CD");
-    let mut port: u16 = 80;
+pub fn proxy(dst_port: u16, dst_address: Ipv4Addr, mut stream: TcpStream) -> io::Result<()>  {
+    stream.set_nodelay(true)?;
 
-    let mut addr = Ipv4Addr::from_str("142.250.183.174").unwrap();
+    let mut socks4_frame: Vec<u8> = Vec::new();
+    socks4_frame.write_u8(4)?;
+    socks4_frame.write_u8(1)?;
+    socks4_frame.write_u16::<BigEndian>(dst_port)?;
+    socks4_frame.write_u32::<BigEndian>(dst_address.into())?;
+    socks4_frame.write_all(UNAME)?;
+    socks4_frame.write_u8(0)?;
 
-
-    socks4Frame.write_u16::<BigEndian>(port).expect("write_u16");
-    socks4Frame.write_u32::<BigEndian>(addr.into()).expect("write_u32");
-    socks4Frame.write_all(user_id).expect("write_all");
-    socks4Frame.write_u8(0).expect("write u8");
-    println!("{:?}", socks4Frame.as_slice());
-
-    stream.write_all(&socks4Frame).expect("TODO: failed to write to sock");
+    stream.write_all(&socks4_frame)?;
 
     let mut response = [0u8; 8];
-    stream.read_exact(&mut response).unwrap();
+    stream.read_exact(&mut response)?;
     let mut response = &response[..];
 
     if response.read_u8().unwrap() != 0 {
-        panic!("invalid response version");
+        return Err(io::Error::new(io::ErrorKind::InvalidData, "invalid response version"));
     }
 
-    println!("{:?}", response);
+    if response[0] != 90 {
+        return Err(io::Error::new(io::ErrorKind::Other, format!("connection failed with status code: {}", response[0])));
+    }
+
+    return Ok(());
 }
 
 
 #[cfg(test)]
 mod tests {
-    use crate::tor_proxy::proxy;
+    use std::net::{Ipv4Addr, TcpStream};
+    use std::str::FromStr;
+    use crate::tor_proxy::{proxy};
+    use crate::types::{ PROXY_ADDR, PROXY_PORT};
 
     #[test]
     fn test_conn() {
-        proxy();
+        let  port: u16 = 80;
+        let  addr = Ipv4Addr::from_str("34.117.118.45").unwrap();
+        let stream = TcpStream::connect(format!("{}:{}", PROXY_ADDR, PROXY_PORT)).unwrap();
+        proxy(port,addr,stream).unwrap();
     }
 }
