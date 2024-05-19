719eb8d6ccbdde26f8ff2b3656d8b9e985b1b35d
Rahul Tripathi <rahultripathidev@gmail.com>
2024-05-19 14:22:07 +0530

    ft: update path

--- toralize/toralize.sh
+++ toralize/toralize.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export DYLD_INSERT_LIBRARIES=/Users/rahultripathi/Desktop/github/toralize-rs/target/debug/libtoralize.dylib
+export DYLD_INSERT_LIBRARIES=$PWD/target/debug/libtoralize.dylib
 
 echo "CUSTOM DYLIB" $DYLD_INSERT_LIBRARIES
 # shellcheck disable=SC2068
