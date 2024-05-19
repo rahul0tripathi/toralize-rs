c8a48956b394e28ec825f945c0e212fefcfe24cc
Rahul Tripathi <rahultripathidev@gmail.com>
2024-05-19 14:08:25 +0530

    ft: add c bindings and rm unsused fields

--- src/types.rs
+++ src/types.rs
@@ -5,6 +5,7 @@ pub const AF_INET: u8 = 2;
 pub const SOCK_STREAM: u8 = 1;
 
 #[repr(C)]
+#[derive(Debug)]
 pub struct SUnB {
     s_b1: libc::c_uchar,
     s_b2: libc::c_uchar,
@@ -13,15 +14,14 @@ pub struct SUnB {
 }
 
 #[repr(C)]
+#[derive(Debug)]
 pub struct SUnW {
     s_w1: libc::c_ushort,
     s_w2: libc::c_ushort,
 }
 
 #[repr(C)]
-pub union SUn {
-    s_un_b: std::mem::ManuallyDrop<SUnB>,
-    s_un_w: std::mem::ManuallyDrop<SUnW>,
+pub struct SUn {
     pub s_addr: libc::c_ulong,
 }
 
@@ -51,4 +51,12 @@ extern "C" {
         len: std::os::raw::c_uint,
     ) -> std::os::raw::c_int;
     pub fn perror(msg: *const libc::c_char);
+    pub fn dup2(old: os::raw::c_int , new: os::raw::c_int) -> os::raw::c_int;
+    pub fn dup(old: os::raw::c_int ) -> os::raw::c_int;
+    pub fn inet_ntoa(ip: libc::c_ulong) -> *const libc::c_char;
 }
+
+
+
+pub const PROXY_ADDR: &str = "127.0.0.1";
+pub const PROXY_PORT: u32 = 9050;
